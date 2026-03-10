1325f333c142530659bd4ce2cbaeb1ab3708ae71
Hoya517 <sv05159@naver.com>
2026-03-10 14:39:20 +0900

    init

---
+++ src/test/java/com/hoya/aicommerce/AiCommercePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hoya.aicommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiCommercePlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
